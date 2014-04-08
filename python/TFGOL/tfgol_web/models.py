2b9fc5d1ec91511474f3db47c52c3a350ff3f75b
Development Server <None@None.None>
2014-04-08 14:31:45 -0400

    started on python models and algorithm

--- python/TFGOL/tfgol_web/models.py
+++ python/TFGOL/tfgol_web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ZipCode(models.Model):
+	population_thousands = models.IntegerField()
+	income_mean  = models.IntegerField()
+	income_stdev = models.IntegerField()
+	crime_permil = models.IntegerField()
+	zip = models.IntegerFIeld(primary_key=True)
+
+
+class Job(models.Model):
+	starting_salary = models.IntegerField()
+	growth_rate = models.DecimalField()
+	name = models.CharField(primary_key=True)
+
+class WorksIn(models.Model):
+	zip = models.ForeignKey('ZipCode')
+	job = models.ForeignKey('Job')
+	percent = models.DecimalField()
